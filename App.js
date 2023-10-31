f934490831b568b3f04d744fcf70b68a6dde15b0
EddyShimwa <shedpray16@gmail.com>
2023-10-31 15:18:02 +0200

    To do list BASIC ui

--- App.js
+++ App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 
 import { StyleSheet, Text, View } from 'react-native';
-
+import Task from './components/Task';
 const App = () => {
   return (
     <View style={styles.container}>
            <View style={styles.taskWrapper}>
-             <Text>React Native</Text>
+             <Text style={styles.sectionTitle}>Today's tasks</Text>
+             <View styles={styles.items}>
+             <Task text={"i am text one"} />
+              <Task text={"i am text two"} />
+            </View>
            </View>
+
+           //
+   
     </View>
   );
 };
@@ -16,7 +23,22 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#E8EAED', 
+  
+  },
+  taskWrapper: {
+      paddingTop: 80,
+      paddingHorizontal: 20,
+
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+
+  items: { 
+    marginTop: 30,
   }
 })
 
